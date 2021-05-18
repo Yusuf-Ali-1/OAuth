abb7b75c515746ab279a1b34f2c491b0391868ef
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-18 11:46:43 -0400

    first commit

--- src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
+++ src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
@@ -19,6 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
+                .antMatchers("/**/*.js", "/**/*.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
