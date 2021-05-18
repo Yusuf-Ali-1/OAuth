006117b07e7e5b443d4e726247a379ec0abfe209
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-18 08:21:20 -0400

    first commit

--- src/main/java/com/tts/authentication/configuration/MvcConfig.java
+++ src/main/java/com/tts/authentication/configuration/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.tts.authentication;
+package com.tts.authentication.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
