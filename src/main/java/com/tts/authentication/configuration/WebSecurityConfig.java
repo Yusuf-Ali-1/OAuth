d1586e7096b793a940adbda3aa8f75b0b1c5147c
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-18 08:48:21 -0400

    first commit

--- src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
+++ src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
@@ -29,6 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
+    // below we are essentially building the details or a user
+    // this method call may say its deprecated but that's a warning
+    // to advise you to not have this code in  production
+
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
